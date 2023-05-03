a4db96460ced9d078f8c622bd6f8efea3def80f9
Abdilova Kamila <kamilaabdilova14@gmail.com>
2023-05-03 21:33:08 +0600

    initial commit

--- src/main/resources/db/migration/V202305032123__createDB.sql
+++ src/main/resources/db/migration/V202305032123__createDB.sql
@@ -1,10 +1,16 @@
--- Create table
-CREATE TABLE user (
-                      id INT PRIMARY KEY,
-                      name VARCHAR(50),
-                      age INT
+CREATE TABLE products
+(
+    id          INT PRIMARY KEY,
+    name        VARCHAR(255)   NOT NULL,
+    description TEXT,
+    price       DECIMAL(10, 2) NOT NULL,
+    image_url   VARCHAR(255)
 );
 
--- Insert test data
-INSERT INTO user (id, name, age) VALUES (1, 'John', 30);
-INSERT INTO user (id, name, age) VALUES (2, 'Mary', 25);
+
+INSERT INTO products (id, name, description, price, image_url)
+VALUES (1, 'Футболка с логотипом', 'Классическая футболка с логотипом на груди', 29.99,
+        'https://example.com/images/product1.jpg'),
+       (2, 'Джинсы', 'Классические джинсы с натуральной посадкой', 49.99, 'https://example.com/images/product2.jpg'),
+       (3, 'Куртка', 'Куртка с капюшоном и застежкой на молнии', 99.99, 'https://example.com/images/product3.jpg');
+
