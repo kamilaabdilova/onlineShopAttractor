0d0acb4170aa15c81c940bc5f837a41e0069e86e
Abdilova Kamila <kamilaabdilova14@gmail.com>
2023-05-04 17:24:55 +0600

    initial commit

--- src/main/resources/db/migration/V202305032123__createDB.sql
+++ src/main/resources/db/migration/V202305032123__createDB.sql
@@ -22,6 +22,6 @@ CREATE TABLE products
 INSERT INTO products (id, name, description, price, image_url)
 VALUES (1, 'Футболка с логотипом', 'Классическая футболка с логотипом на груди', 29.99,
         'https://example.com/images/product1.jpg'),
-       (2, 'Джинсы', 'Классические джинсы с натуральной посадкой', 49.99, 'https://example.com/images/product2.jpg'),
+       (2, 'Джинсы', 'Классические джинсы на высокой посадке', 49.99, 'https://example.com/images/product2.jpg'),
        (3, 'Куртка', 'Куртка с капюшоном и застежкой на молнии', 99.99, 'https://example.com/images/product3.jpg');
 
