796ad4b432e093511f82561c2628aff840e08d7b
Abdilova Kamila <kamilaabdilova14@gmail.com>
2023-05-11 17:24:49 +0600

    added validation

--- src/main/java/com/example/hw65/controllers/UserController.java
+++ src/main/java/com/example/hw65/controllers/UserController.java
@@ -10,21 +10,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @AllArgsConstructor
 @RequestMapping("/users")
 public class UserController {
     private final UserService userService;
     @GetMapping("/findByUserName/{name}")
-    public ResponseEntity<User> findUserByName(@PathVariable String name) {
+    public ResponseEntity<User> findUserByName(@PathVariable @Valid String name) {
         return new ResponseEntity<>(userService.getUserByName(name), HttpStatus.OK);
     }
     @GetMapping("/findByUserAcoount/{account}")
-    public ResponseEntity<User> findUserByAccount(@PathVariable String account) {
+    public ResponseEntity<User> findUserByAccount(@PathVariable @Valid String account) {
         return new ResponseEntity<>(userService.getUserByAccount(account), HttpStatus.OK);
     }
     @GetMapping("/findByUserEmail/{email}")
-    public ResponseEntity<User> findUserByEmail(@PathVariable String email) {
+    public ResponseEntity<User> findUserByEmail(@PathVariable @Valid String email) {
         return new ResponseEntity<>(userService.getUserByEmail(email), HttpStatus.OK);
     }
 }
